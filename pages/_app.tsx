694cf0a81ca34f1615dbcd93a4b2552903e9a224
BisiDavi <oludavidconnect@gmail.com>
2023-04-04 19:11:23 +0100

    schematics editor updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import ProviderLayout from "@/components/ProviderLayout";
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- pages/schematics-editor.tsx
+++ pages/schematics-editor.tsx
@@ -1,11 +1,26 @@
 /* eslint-disable @next/next/no-img-element */
 import Box from "@mui/material/Box";
-
-import Layout from "@/components/Layout";
+import { useState } from "react";
+import Draggable, { DraggableData, DraggableEvent } from "react-draggable";
 import { useAppSelector } from "@/redux/store";
+import Layout from "@/components/Layout";
+
+type Position = {
+  xRate: number;
+  yRate: number;
+};
 
 export default function SchematicsEditor() {
   const { vehicle } = useAppSelector((state) => state.vehicle);
+  const [currentPosition, setCurrentPosition] = useState<Position>({
+    xRate: 150,
+    yRate: 150,
+  });
+
+  const onDrag = (e: DraggableEvent, data: DraggableData) => {
+    setCurrentPosition({ xRate: data.lastX, yRate: data.lastY });
+  };
+
   return (
     <Layout title="Schematics Editor">
       <Box
@@ -20,6 +35,16 @@ export default function SchematicsEditor() {
         {vehicle?.image && (
           <img className="vehicle" src={vehicle?.image} alt={vehicle?.name} />
         )}
+        <Draggable
+          position={{
+            x: currentPosition.xRate,
+            y: currentPosition.yRate,
+          }}
+          defaultClassName=""
+          onDrag={onDrag}
+        >
+          <Box sx={{ color: "black" }}>Hello, I&#39;m Draggable</Box>
+        </Draggable>
       </Box>
       <style jsx>
         {`
