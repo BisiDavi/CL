eafa540310dc256ea548698118576143ff2df09f
BisiDavi <oludavidconnect@gmail.com>
2023-04-04 17:09:15 +0100

    vehicle updated

--- pages/schematics-editor.tsx
+++ pages/schematics-editor.tsx
@@ -1,4 +1,5 @@
 import Box from "@mui/material/Box";
+import Image from "next/image";
 
 import Layout from "@/components/Layout";
 import { useAppSelector } from "@/redux/store";
@@ -8,8 +9,15 @@ export default function SchematicsEditor() {
   return (
     <Layout title="Schematics Editor">
       <Box>
-        <Image src={vehicle.} />
-        </Box>
+        {vehicle?.image && (
+          <Image
+            src={vehicle?.image}
+            height={1000}
+            width={1000}
+            alt={vehicle?.name}
+          />
+        )}
+      </Box>
     </Layout>
   );
 }
